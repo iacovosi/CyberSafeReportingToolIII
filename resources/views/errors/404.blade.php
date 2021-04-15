96006384760ed961a414592a499cba846479f2a5
Demetris Kaizer <demetriskaizer@pop-os.localdomain>
2021-04-15 13:33:57 +0300

    fixing 404 error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.app')
 
-    @section('content')
+@section('content')
 
-    <div class="container">
+<div class="container">
 
 
-     <div class="row">
-            <div class="col-md-12">
-                Oups... not good browsing (404)!
-            </div>
-      </div>
-
-    @endsection
-
+    <div class="row">
+        <div class="col-md-12">
+            Oups... not good browsing (404)!
+        </div>
+    </div>
 
 @endsection
