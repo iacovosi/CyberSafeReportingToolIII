266c2f917a823b2b1dce400446e85081f29ccdf3
Demetris Kaizer <demetriskaizer@pop-os.localdomain>
2021-04-27 21:10:01 +0300

    fixes to queries for laravel 5.8

--- resources/views/helpline/showmanager.blade.php
+++ resources/views/helpline/showmanager.blade.php
@@ -13,12 +13,11 @@
                         <div class="panel-heading clearfix">
                             <h4 class="pull-left">HELPLINE - Report ID : # {{ $helpline->id }}</h4>
                             <div class="pull-right form-actions">
-                                <a href="{{route('hotline.edit',['id' => $helpline->id])}}" class="btn btn-danger"><i
-                                            class="fa fa-paper-plane-o" aria-hidden="true"></i> Forward to HOTLINE</a>
+                                {{-- <a href="{{route('hotline.edit',['id' => $helpline->id])}}" class="btn btn-danger"><i
+                                            class="fa fa-paper-plane-o" aria-hidden="true"></i> Forward to HOTLINE</a> --}}
                                 <a href="" class="btn btn-default" data-toggle="modal" data-target="#myModal"><i
                                             class="fa fa-times" aria-hidden="true"></i> Cancel</a>
                                 @role('manager')
-                                    {{--  <input type="submit" name="submit" value="Save & Exit" form="submit-form" class="btn btn-primary">  --}}
                                     <button type="submit" class="btn btn-primary">
                                         <i class="fa fa-floppy-o" aria-hidden="true"></i> Save & Exit
                                     </button>
