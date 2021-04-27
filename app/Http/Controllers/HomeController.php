393e4b0032646bd406f2893a0d9b93311c1c158c
Demetris Kaizer <demetriskaizer@pop-os.localdomain>
2021-04-27 21:10:01 +0300

    fixes to queries for laravel 5.8

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,146 +41,72 @@ class HomeController extends Controller
         $references_by = ReferenceBy::all();
         $references_to = ReferenceTo::all();
 
-        // $actionsTaken = ActionTaken::all();
-        // $actions = ActionTaken::all();
-        // if (count($request->actionsTaken)>0){
-        //     $act = $request->actionsTaken;
-        // } else {
-        //     $act = $actions;
-        // }
         $request->flash();
 
-        // --------------- //
         // APPLY FILTERING //
         if ($request->filterStatus) {
             $statusSelected = $request->filterStatus;
 
             // ...set filters but dont get resuts yet, due to pagination and export conflict
-            //check user permissions
             $user = auth()->user();
-            $IsUserOperator=$user->hasRole("operator") ;
-            // dd($IsUserManager);
-            if (!$IsUserOperator) {
+
+
+            // admin & manager get all reports.
+            if ($user->hasRole("admin") || $user->hasRole("manager")) {
                 $helpline = Helpline::ofStatus($statusSelected)->get();
-                // $helpline = Helpline::whereStatus($statusSelected)->get();
             }
             else {
                 if ($statusSelected!="*") {
-                    $helpline = Helpline::where('status', '=', $statusSelected)
-                        ->where(function ($query) {
-                            $query->where('user_assigned', Auth::id())
+                    $helpline = Helpline::where(function ($query) use ($statusSelected) {
+                        $query->select('*')
+                                ->where('status', '=', $statusSelected)
+                                ->where('user_assigned', Auth::id())
                                 ->orwhere('user_assigned', NULL)
-                                ->orwhere('forwarded', "true");
-                        })->where(function($query) {
-                             $query->where('user_opened', Auth::id())
-                                ->orwhere('user_opened',NULL)
-                                ->orwhere('forwarded', "true");
-                        })
-                        ->get();
-                }
-                else {
-                    $helpline = Helpline::ofStatus("*")->where(function ($query) {
-                            $query->where('user_assigned', Auth::id())
-                                ->orwhere('user_assigned', NULL)
-                                ->orwhere('forwarded', "true");
-                    })->where(function($query) {
-                            $query->where('user_opened', Auth::id())
+                                ->orwhere('user_opened', Auth::id())
                                 ->orwhere('user_opened', NULL)
                                 ->orwhere('forwarded', "true");
-                        })
-                        ->get();
+                    })->get();
+                }
+                else {
+                    $helpline = Helpline::where(function ($query) {
+                        $query->select('*')
+                              ->where('user_assigned', Auth::id())
+                              ->orwhere('user_assigned', NULL)
+                              ->orwhere('user_opened', Auth::id())
+                              ->orwhere('user_opened', NULL)
+                              ->orwhere('forwarded', "true");
+                    })->get();
                 }
             }
         } else {
-            //check user permissions
+
             $user = auth()->user();
-            $IsUserOperator=$user->hasRole("operator") ;
-            // dd($IsUserManager);
-            if (!$IsUserOperator) {
-                // ...set filters but dont get resuts yet, due to pagination and export conflict
+            
+            // admin & manager can view everything.
+            if ($user->hasRole("admin") || $user->hasRole("manager")) {
                 $helpline = Helpline::ofStatus("*")->get();
             }
             else {
-                $helpline = Helpline::where('status','!=','Closed')
-                ->where(function ($query) {
-                    $query->where('user_assigned',Auth::id())
-                        ->orwhere('user_assigned',NULL)
-                        ->orwhere('forwarded', "true");
-                        })->where(function($query) {
-                        $query->where('user_opened',Auth::id())
-                        ->orwhere('user_opened',NULL)
-                        ->orwhere('forwarded', "true");
-                    })
-                ->get();
+                $helpline = Helpline::where(function ($query) {
+                    $query->select('*')
+                          ->where('user_assigned', Auth::id())
+                          ->orwhere('user_assigned', NULL)
+                          ->orwhere('user_opened', Auth::id())
+                          ->orwhere('user_opened', NULL)
+                          ->orwhere('forwarded', "true");
+                })->get();
+
             }
         }
 
         return view('home')->with([
-                    'helpline'=> $helpline,
-                    // 'hotline' => $hotline,
-                    'resource_types' => $resource_types,
-                    'content_types' => $content_types,
-                    'references_by' => $references_by,
-                    'references_to' => $references_to,
-                    'status' => $status,
-                    'users' => $users
-                ]);
-
-        // if ($status->contains('name',$request->sortStatus) || $request->sortStatus == "*") {
-
-        //     if($request->sortStatus == "*") {
-        //         if($request->sortUser == "*" && $request->sortUser=="default" || $request->sortUser=="*"){
-        //             $helpline = Helpline::all();
-        //         } else if($request->sortUser != "default") {
-        //             $helpline = Helpline::where('user_id',$request->sortUser)->get();
-        //         } else if($request->sortUser == "*" && $request->sortUser=="default" ) {
-        //         $helpline = Helpline::all();
-        //         }
-        //     } else if ($request->sortStatus != "*" && $request->sortUser =="default" || $request->sortUser=="*") {
-        //         $helpline = Helpline::where('status',$request->sortStatus)->get();
-        //     } else if ($request->sortStatus != "*" && $request->sortUser !="default" && $request->sortUser!="*") {
-        //         $helpline = Helpline::where('status',$request->sortStatus)->where('user_id',$request->sortUser)->get();
-        //     } else {
-        //         $helpline = Helpline::all();
-        //     }
-
-        //     $hotline = Hotline::all();
-        //     // $resource_types = ResourceType::all();
-        //     // $content_types  = ContentType::all();
-
-        //     return view('home')->with([
-        //         'helpline'=> $helpline, 
-        //         'hotline' => $hotline,
-        //         'resource_types' => $resource_types, 
-        //         'content_types' => $content_types,
-        //         'references_by' => $references_by, 
-        //         'references_to' => $references_to,
-        //         'status' => $status, 
-        //         'users' => $users , 
-        //         'actions' => $actions
-        //     ]);
-
-        // } else {
-        //     $hotline = Hotline::all();
-
-        //     $helpline = Helpline::where('status','!=','Closed')
-        //         ->where(function ($query) {
-        //             $query->where('user_assigned',Auth::id())
-        //                   ->orwhere('user_assigned',NULL)
-        //                   ->where('user_opened',Auth::id())
-        //                   ->orwhere('user_opened',NULL);
-        //             })
-        //         ->get();
-
-        //     // $resourcetypes = ResourceType::all();
-        //     // $contenttypes  = ContentType::all();
-
-        //     return view('home')->with([
-        //         'helpline'=> $helpline , 'hotline' => $hotline,
-        //         'resource_types' => $resource_types, 'content_types' => $content_types,
-        //         'references_by' => $references_by, 'references_to' => $references_to,
-        //         'status' => $status, 'users' => $users, 'actions' => $actions
-        //     ]);
-        // }
+            'helpline'=> $helpline,
+            'resource_types' => $resource_types,
+            'content_types' => $content_types,
+            'references_by' => $references_by,
+            'references_to' => $references_to,
+            'status' => $status,
+            'users' => $users
+        ]);
     }
 }
