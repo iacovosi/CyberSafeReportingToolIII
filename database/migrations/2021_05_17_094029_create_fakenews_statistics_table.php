96b8665d59b33d3330b29fe4261e35689b762047
OrestisN <49041261+OrestisN@users.noreply.github.com>
2021-05-20 11:41:50 +0300

    Revert "Revert "Homeciew works completely now except fakenews statistics""
    This reverts commit 12fb722e5f39033afa1a4a40ad8ddab596c96a1b.

--- database/migrations/2021_05_17_094029_create_fakenews_statistics_table.php
+++ database/migrations/2021_05_17_094029_create_fakenews_statistics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatisticsTable extends Migration
+class CreateFakenewsStatisticsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,39 +13,39 @@ class CreateStatisticsTable extends Migration
      */
     public function up()
     {
-        Schema::create('statistics', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('fakenews_statistics', function (Blueprint $table) {
+            $table->bigIncrements('id');
 
             $table->integer('tracking_id');
-            $table->string('is_it_hotline')->nullable();
-            $table->string('forwarded')->nullable();
             $table->string('submission_type')->nullable();
             // user profile
             $table->string('age')->nullable();
             $table->string('gender')->nullable();
-            $table->string('report_role')->nullable();            
-            // report description
-            $table->string('resource_type');
-            $table->string('content_type');
+            $table->string('report_role')->nullable();
+            // fakenews report description
+            $table->string('fakenews_source_type');
+            $table->string('fakenews_type');
+            $table->string('evaluation');
+            $table->boolean('img_upload');
+            // location info available?
+            $table->boolean('loc_available');
             // operator actions
             $table->integer('user_opened')->unsigned()->nullable();
             $table->foreign('user_opened')->references('id')->on('users');
             $table->unsignedInteger('user_assigned')->nullable();
             $table->foreign('user_assigned')->references('id')->on('users');
-            //
+            //admin info
             $table->string('priority')->nullable();
             $table->string('reference_by')->nullable();
             $table->string('reference_to')->nullable();
             $table->longtext('actions')->nullable();
             $table->string('status');
-            //
-            $table->string('job')->nullable();
-            $table->string('form_type')->nullable();
             //manager comments
             $table->text('manager_comments')->nullable();
+
             //field to connect insident with another
-            $table->unsignedInteger('insident_reference_id')->nullable(); // insident_id
-            $table->foreign('insident_reference_id')->references('id')->on('helplines'); // insident_id
+            $table->BigInteger('insident_reference_id')->unsigned()->nullable();         // insident_id
+            $table->foreign('insident_reference_id')->references('id')->on('fakenews')->onDelete('cascade'); // insident_id  
             //the call time
             $table->dateTime('call_time')->nullable();
 
@@ -60,6 +60,6 @@ class CreateStatisticsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('statistics');
+        Schema::dropIfExists('fakenews_statistics');
     }
 }
